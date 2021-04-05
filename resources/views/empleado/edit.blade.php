b961529b9522da8e0fec7fd363d71463bb4dbefd
Diego Urdanigo <diegourdanigo@gmail.com>
2021-04-05 16:30:21 -0500

    actualizacionfinal

--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -1,2 +1,6 @@
 formulario de edicion de empleado
+<form action="{{ url('/empleado/'.$empleado->id }}" method="post" enctype ="multipart/form-data">
+@csrf 
+{{method-fiel('PATCH')}}
 @include('empleado.form');
+</form>
